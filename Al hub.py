930012de72e33357cbc73d7bb23766ae2c4759f3
Titiotis1 <52955356+Titiotis1@users.noreply.github.com>
2019-07-19 11:08:30 +0100

    Add files via upload

---
+++ Al hub.py
@@ -0,0 +1,6 @@
+cgpa = float(input("What is your cgpa? "))
+credit=int(input("What is your credit?"))
+if credit >= 180 and cgpa = 3.5:
+    print("You are eligible for masters ")
+else:
+    print("retake your exams")
